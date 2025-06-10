1e7b84f9044743ada02f40f9f45831bccf8dd85d
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-06-10 15:21:08 -0300

    feat: adicionar componente FramerMagnetic com efeito magnético

---
+++ src/components/ui/FramerMagnetic.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { motion } from "motion/react";
+import { useRef, useState } from "react";
+
+export function FramerMagnetic({ children }: { children: React.ReactNode }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const mouseMove = (e: any) => {
+    const { clientX, clientY } = e;
+    if (!ref.current) return;
+
+    const { width, height, left, top } = ref.current.getBoundingClientRect();
+    const x = clientX - (left + width / 2);
+    const y = clientY - (top + height / 2);
+    setPosition({ x, y });
+  };
+
+  const mouseLeave = (e: any) => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  return (
+    <motion.div
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      ref={ref}
+      animate={{ x: position.x, y: position.y }}
+      transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+// animation with magnetic effect
