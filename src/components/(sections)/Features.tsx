f448db544c9cf93b8c5e3dfca3e3726a01bb5c69
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-06-11 17:47:51 -0300

    feat:finalizando component Feature e adicionando efeito em group nos Cards/>

--- src/components/(sections)/Features.tsx
+++ src/components/(sections)/Features.tsx
@@ -10,6 +10,7 @@ import avatar2 from "@/assets/images/avatar-florence-shaw.jpg";
 import avatar3 from "@/assets/images/avatar-lula-meyers.jpg";
 import avatar4 from "@/assets/images/avatar-owen-garcia.jpg";
 import Image from "next/image";
+import { FramerMagnetic } from "../ui/FramerMagnetic";
 
 export function Features() {
   const features = [
@@ -53,6 +54,7 @@ export function Features() {
             <Card
               paragraph="Work together seamlessly with conflict-free team editing"
               title="Real time collaboration"
+              className="group"
             >
               {/* avatar div */}
               <div className="aspect-video flex items-center justify-center">
@@ -79,19 +81,24 @@ export function Features() {
                     className="rounded-full"
                   />
                 </Avatar>
-
-                <Avatar className="-ml-6 border-transparent z-10">
-                  <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1 relative">
-                    <Image src={avatar4} alt="avatar 4" className="size-full absolute rounded-full"/>
-                    {/*render 3 span tags*/}
-                    {Array.from({ length: 3 }).map((_, i) => (
-                      <span
-                        key={i}
-                        className="size-1.5 bg-white rounded-full inline-flex"
-                      ></span>
-                    ))}
-                  </div>
-                </Avatar>
+                <FramerMagnetic>
+                  <Avatar className="-ml-6 border-transparent z-10 group-hover:border-lime-500 transition">
+                    <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1 relative">
+                      <Image
+                        src={avatar4}
+                        alt="avatar 4"
+                        className="size-full absolute rounded-full opacity-0 group-hover:opacity-100 transition duration-500"
+                      />
+                      {/*render 3 span tags*/}
+                      {Array.from({ length: 3 }).map((_, i) => (
+                        <span
+                          key={i}
+                          className="size-1.5 bg-white rounded-full inline-flex"
+                        ></span>
+                      ))}
+                    </div>
+                  </Avatar>
+                </FramerMagnetic>
               </div>
             </Card>
           </motion.div>
@@ -103,12 +110,21 @@ export function Features() {
             <Card
               paragraph="Engage your clients with prototypes that react to user actions"
               title="Interactive Prototyping"
+              className="group"
             >
               <div className="aspect-video flex items-center justify-center">
-                <p className="text-4xl font-extrabold text-white/20 text-center">
+                <p className="text-4xl font-extrabold text-white/20 text-center group-hover:text-white/10 transition duration-300">
                   We&apos;ve achieved{" "}
-                  <span className="bg-gradient-to-r from-purple-400 to-indigo-500 text-transparent bg-clip-text hover:from-indigo-500 hover:to-purple-400 duration-300 transition-colors">
-                    incredible
+                  <span className="bg-gradient-to-r from-purple-400 to-indigo-500 text-transparent bg-clip-text hover:from-indigo-500 hover:to-purple-400 duration-300 transition-colors relative">
+                    <span>incredible</span>
+                    <video
+                      src={"/assets/gif-incredible.mp4"}
+                      autoPlay
+                      loop
+                      muted
+                      playsInline
+                      className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl opacity-0 pointer-events-none group-hover:opacity-100 transition duration-500"
+                    />
                   </span>{" "}
                   growth this year
                 </p>
@@ -123,11 +139,18 @@ export function Features() {
             <Card
               title="Keyboard Quick Actions"
               paragraph="Powerful commands to help you create designs more quickly"
+              className="group"
             >
               <div className="aspect-video flex items-center justify-center gap-4">
-                <Key className="w-28">shift</Key>
-                <Key>alt</Key>
-                <Key>C</Key>
+                <Key className="w-28 outline-2 outline-transparent group-hover:outline-lime-400 outline-offset-4 translate-y-1 group-hover:translate-y-0 transition-all duration-500">
+                  shift
+                </Key>
+                <Key className="outline-2 outline-transparent group-hover:outline-lime-400 outline-offset-4 translate-y-1 group-hover:translate-y-0 transition-all duration-500 delay-150">
+                  alt
+                </Key>
+                <Key className="outline-2 outline-transparent group-hover:outline-lime-400 outline-offset-4 translate-y-1 group-hover:translate-y-0 transition-all duration-500 delay-300">
+                  C
+                </Key>
               </div>
             </Card>
           </motion.div>
