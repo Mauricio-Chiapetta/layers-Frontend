9e27e59f0fc92d062707012d1329a429fbc8e735
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-06-17 18:49:03 -0300

    feat: add lenis package to smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { CallToAction } from "@/components/(sections)/CallToAction";
 import { Faqs } from "@/components/(sections)/Faqs";
 import { Features } from "@/components/(sections)/Features";
@@ -8,8 +9,19 @@ import { Introduction } from "@/components/(sections)/Introduction";
 import { LogoTicker } from "@/components/(sections)/LogoTicker";
 import { Navbar } from "@/components/(sections)/Navbar";
 import { ScrollLinked } from "@/components/ui/ScrollLinked";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <ScrollLinked />
